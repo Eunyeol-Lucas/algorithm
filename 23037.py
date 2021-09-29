1bfb4b2e2a0ca06dbc2bba11beb3a3a795d34dee
Eunyeol <ney9083@gmail.com>
2021-09-29 23:29:18 +0900

    20210929

---
+++ 23037.py
@@ -0,0 +1,7 @@
+_list = list(map(int, input()))
+
+sum = 0
+for i in _list:
+    sum += i**5
+
+print(sum)
--- 백트래킹/15652.py
+++ 백트래킹/15652.py
@@ -1,18 +1,22 @@
 N, M = map(int, input().split())
 visited = [False] * N
 out = []
+num = 0
 
 def backtracking(depth,idx, N, M):
     if depth == M:
-        # print(depth,idx, N, M)
+        print(depth,idx, N, M)
         print(" ".join(map(str, out)))
         return 
+    num += 1
     for i in range(idx, N):
+        
         if not visited[i]:
             visited[i] = True
             out.append(i+1)
             visited[i] = False
-            backtracking(depth +1, i, N, M)
+            
+            backtracking(depth +1, i, N, M, num)
             
             out.pop()
 
