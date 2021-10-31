7c8697c1974c6144d20ac31f29f07fcbf14c02ba
Eunyeol <ney9083@gmail.com>
2021-11-01 08:57:51 +0900

    [20211101 [프로그래머스-연습_힙_더 맵게]

---
+++ 프로그래머스/연습_힙_더 맵게.py
@@ -0,0 +1,13 @@
+
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+    print("s1", scoville)
+    while len(scoville) >= 2 and scoville[0] < K:
+        answer += 1
+        num1, num2 = heapq.heappop(scoville), heapq.heappop(scoville)
+        heapq.heappush(scoville, num1 + num2 * 2)
+
+    return answer if scoville[0] >= K else -1
