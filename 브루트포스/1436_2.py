754199a7c3e56fedfbfd196cd3aaff4122d28b4b
Eunyeol <ney9083@gmail.com>
2021-11-03 08:21:12 +0900

    20211103 [백준-영화감독 숌]

---
+++ 브루트포스/1436_2.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+six_3 = 666
+cnt = 0
+
+while True:
+    if '666' in str(six_3):
+        cnt += 1
+    if cnt == n:
+        print(six_3)
+        break
+    six_3 += 1
