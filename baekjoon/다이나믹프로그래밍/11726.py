f89b5e3dc920af087632c49b02e353e590ff88f0
Eunyeol <ney9083@gmail.com>
2022-02-10 10:12:32 +0900

    Solve: 백준-2xn 타일링

--- baekjoon/다이나믹프로그래밍/11726.py
+++ baekjoon/다이나믹프로그래밍/11726.py
@@ -1,16 +1,14 @@
 import sys
 
-def get_number_of_fills(n):
-    arr = [0] * (n+1)
-    # 2 * 1을 채우는 방법은 1가지
-    arr[1] = 1
-    # 2 *2를 채우는 방법 2가지
-    arr[2] = 2
-    # 3이상부터 다이나믹 프로그래밍 규칙에 따라 i는 i - 1의 방법의 수와 i - 2의 방법의 수의 합
-    for i in range(3, n+1):
-        arr[i] = arr[i-1] + arr[i-2]
-    return arr[n] % 10007
-
-
 n = int(sys.stdin.readline())
-print(get_number_of_fills(n))
+arr = [0] * 1001
+# 2 * 1을 채우는 방법은 1가지
+arr[1] = 1
+# 2 *2를 채우는 방법 2가지
+arr[2] = 2
+# 3이상부터 다이나믹 프로그래밍 규칙에 따라 i는 i - 1의 방법의 수와 i - 2의 방법의 수의 합
+for i in range(3, 1001):
+    arr[i] = arr[i-1] + arr[i-2]
+print(arr[n] % 10007)
+
+
