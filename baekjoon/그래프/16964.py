7867f5e17549d87eae0d2ed56f951e55ddcccee0
Eunyeol <ney9083@gmail.com>
2022-03-27 03:13:22 +0900

    Resolve: 백준-DFS 스페셜 저지

--- baekjoon/그래프/16964.py
+++ baekjoon/그래프/16964.py
@@ -1,27 +1,17 @@
 import sys
-sys.setrecursionlimit(10**9)
+from collections import deque
 input = sys.stdin.readline
 
-def dfs(x, t):
-    global idx, flag
-    idx += 1
-    if len(answer) == N:
-        return
-    for i in graph[x]:
-        if not vst[i]:
-            t.append(i)
-    while t and idx < N:
-        for k in range(len(t)):
-            if idx < N and not vst[t[k]]:
-                if t[k] == order[idx]:
-                    a = t.pop(k)
-                    vst[a] = 1
-                    answer.append(a)
-                    dfs(a, [])
-                    break
-        else:
-            exit()
-
+def dfs():
+    x = stk.popleft()
+    if not stk:
+        print(1)
+        exit()
+    vst[x] = 1
+    for i in range(len(graph[x])):
+        if stk[0] in graph[x] and not vst[stk[0]]:
+            dfs()
+    return 0
 
 N = int(input())
 graph = [[] for _ in range(N+1)]
@@ -29,18 +19,18 @@ for i in range(N-1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-order = list(map(int, input().split()))
 vst = [0] * (N+1)
-if order[0] != 1:
+order = list(map(int, input().split()))
+
+stk = deque(order)
+if stk[0] != 1:
     print(0)
     exit()
-answer = [order[0]]
-vst[1] = 1
-idx = 0
-flag = 0
-dfs(1, [])
 
-print(1 if answer == order else 0)
+if not dfs():
+    print(0)
+
+
 
 '''
 7
@@ -52,3 +42,18 @@ print(1 if answer == order else 0)
 4 7
 1 5 3 6 2 4 7
 '''
+
+'''
+9
+1 1
+1 2
+1 3
+1 4 
+1 5
+1 6
+1 7
+1 8
+1 2 3 4 5 6 7 8 
+'''
+
+
