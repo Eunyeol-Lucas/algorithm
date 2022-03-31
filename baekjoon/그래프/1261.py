9cbb3ec51595904b85279060267f4e8e1efbd3b2
Eunyeol <ney9083@gmail.com>
2022-03-31 23:30:09 +0900

    Solve: 백준-알고스팟

--- baekjoon/그래프/1261.py
+++ baekjoon/그래프/1261.py
@@ -1,5 +1,35 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-M, N = map(int, input().split()) # 가로, 세로
-arr = [list(map(int, input().split())) for _ in range(N)]
+
+def bfs():
+    global answer
+    q = deque()
+    q.append((0, 0))
+    vst = [[-1] * (M) for _ in range(N)]
+    vst[0][0] = 0
+    while q:
+        x, y = q.popleft()
+        if x == N-1 and y == M-1:
+            print(vst[x][y])
+            exit()
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < N and 0 <= ny < M and vst[nx][ny] == -1:
+                # 빈방일 경우 그냥 지나칠 수 있으므로, 직전까지 벽을 뚫은 숫자를 그대로 가져간다.
+                if arr[nx][ny] == 0:
+                    vst[nx][ny] = vst[x][y]
+                    # 빈방으로 이동하여 목적지까지 도착하는 경우가 가장 적게 벽을 뚫은 숫자이므로,
+                    # 최소값을 찾기 위해 queue의 앞으로 추가한다.
+                    q.appendleft((nx, ny))
+                else:
+                    # 벽일 경우 뚫고 지나가야하므로 직전까지 벽을 뚫은 숫자 + 1을 추가한다.
+                    vst[nx][ny] = vst[x][y] + 1
+                    q.append((nx, ny))
+
+
+M, N = map(int, input().split())  # 가로, 세로
+arr = [list(map(int, input().strip())) for _ in range(N)]
+bfs()
--- baekjoon/그래프/14226.py
+++ baekjoon/그래프/14226.py
@@ -1,11 +1,16 @@
 from collections import deque
 # solution 1
+
+
 def bfs():
     q = deque([(1, 0)])  # 이모티콘 수, 클립보드 이모티콘 수
     vst[1][0] = 0
     while q:
         x, y = q.popleft()
-        if vst[x][x] == -1: # 방문하지 않은 경우
+        if x == N:
+            print(vst[x][y])
+            exit()
+        if vst[x][x] == -1:  # 방문하지 않은 경우
             # 화면에 있는 이모티콘을 모두 복사해서 클립보드에 저장
             vst[x][x] = vst[x][y] + 1
             q.append((x, x))
@@ -23,39 +28,33 @@ N = int(input())
 vst = [[-1] * (N+1) for _ in range(N+1)]
 bfs()
 
-answer = -1
-for i in range(N+1):
-    if vst[N][i] != -1:
-        if answer == -1 or answer > vst[N][i]:
-            answer = vst[N][i]
-
 # print(answer)
 
 # solution 2
 
 
-def bfs(n, c): # 화면의 이모티콘 수와, 클립보드에 저장된 이모티콘의 수를 딕셔너리에 저장
-    vst[(n, c)] = True 
-    dist[(n, c)] = 0
-    q = deque([(n, c)])
+# def bfs(n, c): # 화면의 이모티콘 수와, 클립보드에 저장된 이모티콘의 수를 딕셔너리에 저장
+#     vst[(n, c)] = True
+#     dist[(n, c)] = 0
+#     q = deque([(n, c)])
 
-    while q:
-        s, c = q.popleft()
-        if s == N:
-            return dist[(s, c)]
-        
-        for i in [(s, s), (s+c, c), (s-1, c)]: # 복사하는 경우, 클립보드에 있는 모든 이모티콘을 화면에 붙여넣는 경우, 이모티콘 중 하나를 삭제하는 경우
-            if i[0] < 0: #화면의 이모티콘 수가 0 미만인 경우 continue
-                continue
-            if vst.get(i, False): # 방문한적 있는 경우 continue
-                continue
-            q.append(i) # 이모티콘 수와 클립보의 이모티콘의 수를 q에 추가
-            vst[i] = True # 방문 처리
-            dist[i] = dist[(s, c)] + 1 # 시간 저장
+#     while q:
+#         s, c = q.popleft()
+#         if s == N:
+#             return dist[(s, c)]
+
+#         for i in [(s, s), (s+c, c), (s-1, c)]: # 복사하는 경우, 클립보드에 있는 모든 이모티콘을 화면에 붙여넣는 경우, 이모티콘 중 하나를 삭제하는 경우
+#             if i[0] < 0: #화면의 이모티콘 수가 0 미만인 경우 continue
+#                 continue
+#             if vst.get(i, False): # 방문한적 있는 경우 continue
+#                 continue
+#             q.append(i) # 이모티콘 수와 클립보의 이모티콘의 수를 q에 추가
+#             vst[i] = True # 방문 처리
+#             dist[i] = dist[(s, c)] + 1 # 시간 저장
 
 
-N = int(input())
+# N = int(input())
 
-vst = {} # 임티수, 클립보드 임티 수 체크
-dist = {} # 시간을 저장하는 딕셔너리
-print(bfs(1, 0))
+# vst = {} # 임티수, 클립보드 임티 수 체크
+# dist = {} # 시간을 저장하는 딕셔너리
+# print(bfs(1, 0))
