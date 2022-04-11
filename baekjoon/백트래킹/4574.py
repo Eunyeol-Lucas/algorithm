6c822b923c4019512c1672988accc9f05b77c5a1
Eunyeol <ney9083@gmail.com>
2022-04-11 23:49:08 +0900

    Solve: 백준-스도노미쿠

--- baekjoon/백트래킹/4574.py
+++ baekjoon/백트래킹/4574.py
@@ -1,6 +1,8 @@
 import sys
 input = sys.stdin.readline
 
+turn = 1
+
 
 def check(x, y, tmp):
     for i in range(9):
@@ -17,49 +19,50 @@ def check(x, y, tmp):
     return True
 
 
-def go(sudomino):
+def dfs(lst):
     global flag
-    if len(sudomino) == 0:
+    if len(lst) == 0:
         flag = 1
-        print('Puzzle', idx)
-        for i in arr:
-            print(i)
-        return
-    if flag == 1:
-        return
-    for i in range(9):
-        for j in range(9):
-            if arr[i][j] == 0:
-                x, y = i, j
-                for n, m in sudomino:
-                    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                        nx, ny = x + dx, y + dy
-                        if 0 <= nx < 9 and 0 <= ny < 9 and arr[nx][ny]:
-                            tmp = sudomino[:]
-                            re_arr = (n, m)
-                            tmp.remove(re_arr)
+        print('Puzzle', turn)
+        for u in range(9):
+            for v in range(9):
+                print(arr[u][v], end='')
+            print()
+        return 1
+    if not flag:
+        # 도미노를 넣은 빈칸을 탐색
+        for i in range(9):
+            for j in range(9):
+                if arr[i][j] == 0:
+                    x, y = i, j
+                    break
+        # 빈 칸에 모든 도미노를 넣어 봄
+        for n, m in lst:
+            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < 9 and 0 <= ny < 9 and not arr[nx][ny]:
+                    tmp = lst[:]
+                    tmp.remove((n, m))
+                    if check(x, y, n) and check(nx, ny, m):
+                        arr[x][y], arr[nx][ny] = n, m
+                        if flag == 1:
+                            return
+                        dfs(tmp)
+                        arr[x][y], arr[nx][ny] = 0, 0
+                    if check(x, y, m) and check(nx, ny, n):
+                        arr[x][y], arr[nx][ny] = m, n
+                        if flag == 1:
+                            return
+                        dfs(tmp)
+                        arr[x][y], arr[nx][ny] = 0, 0
 
-                            if check(x, y, n) and check(nx, ny, m):
-                                arr[x][y], arr[nx][ny] = n, m
-                                if flag == 1:
-                                    return
-                                go(tmp)
-                                arr[x][y], arr[nx][ny] = 0, 0
-                            if check(x, y, m) and check(x, y, n):
-                                arr[x][y], arr[nx][ny] = m, n
-                                if flag == 1:
-                                    return
-                                go(tmp)
-                                arr[x][y], arr[nx][ny] = 0, 0
 
-
-idx = 1
 while True:
+    arr = [[0] * 9 for _ in range(9)]
     N = int(input())
-    print(N)
+
     if N == 0:
         break
-    arr = [[0] * 9 for _ in range(9)]
 
     domino = []
     for i in range(1, 10):
@@ -70,17 +73,17 @@ while True:
         U, LU, V, LV = input().split()
         U = int(U)
         V = int(V)
-        x1, y1 = ord(LU[0]) - ord('A'), int(LU[1]) - 1
-        x2, y2 = ord(LV[0]) - ord('A'), int(LV[1]) - 1
+        x1, y1 = ord(LU[0]) - 65, int(LU[1]) - 1
+        x2, y2 = ord(LV[0]) - 65, int(LV[1]) - 1
         arr[x1][y1], arr[x2][y2] = U, V
         if V < U:
             V, U = U, V
         domino.remove((U, V))
 
     for i, v in enumerate(input().split()):
-        x, y = ord(v[0]) - ord('A'), int(v[1]) - 1
+        x, y = ord(v[0]) - 65, int(v[1]) - 1
         arr[x][y] = i + 1
 
     flag = 0
-    go(domino)
-    idx += 1
+    dfs(domino)
+    turn += 1
